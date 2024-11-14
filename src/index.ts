ad0d95ffa81e30278d61b7bad084017ced5732fe
agrippa <afsalahamed07@gmail.com>
2024-11-14 12:19:02 +0530

    complete assignment

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-import { generateRandomNumber } from "./random-number.js";
+import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 
-console.log(generateRandomNumber());
+const PORT = process.env.PORT;
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer(async (req, res) => {
+	try {
+		if (req.method === "GET") {
+			let filePath;
+			if (req.url === "/") {
+				filePath = path.join(__dirname, "index.html")
+			} else if (req.url == "/about") {
+				filePath = path.join(__dirname, "about.html")
+			} else if (req.url == "/contact") {
+				filePath = path.join(__dirname, "contact.html")
+			} else {
+				filePath = path.join(__dirname, "404.html")
+			}
+			const data = await fs.readFile(filePath);
+			res.setHeader("Content-Type", "text/html");
+			res.write(data);
+			res.end();
+
+		} else {
+			throw new Error("Not allowed")
+		}
+	} catch (error) {
+		res.writeHead(500, { 'Content-Type': 'text/plain' })
+		res.write("Server Error");
+	} finally {
+		res.end();
+	}
+})
+
+server.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
--- src/random-number.ts
+++
@@ -1,5 +0,0 @@
-function generateRandomNumber() {
-	return Math.floor(Math.random() * 100) + 1;
-}
-
-export { generateRandomNumber }
